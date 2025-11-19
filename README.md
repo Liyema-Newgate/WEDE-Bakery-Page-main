63d928c82b969c49353ccc48d9f4c33994ced63d
Liyema-Newgate <ST10494709@rcconnect.edu.za>
2025-11-20 00:55:59 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# WEDE-Bakery-Page
+The Wendy's Sweet Treats website page for part 1 & 2 of WEDE
+
+
+Edited POE Part 1 proposal to include feedback to name pages. 
+added css styling following instructions in parts 2, simple to read and navigate, will adjust to different screen sizes and 
+decided on a light blue colour scheme.
+
+Code Link: https://github.com/Liyema-Newgate/WEDE-Bakery-Page.git
---
+++ script.js
@@ -0,0 +1,35 @@
+// Load cart or create empty one
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
+
+function loadCart() {
+    let container = document.getElementById("cart-items");
+    let total = 0;
+
+    container.innerHTML = "";
+
+    cart.forEach((item, index) => {
+        total += item.price;
+
+        container.innerHTML += `
+            <div class="cart-item">
+                <h3>${item.name}</h3>
+                <p>Price: R${item.price}</p>
+                <button onclick="removeItem(${index})">Remove</button>
+            </div>
+        `;
+    });
+
+    document.getElementById("cart-total").innerText = "Total: R" + total;
+}
+
+function removeItem(index) {
+    cart.splice(index, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    loadCart();
+}
